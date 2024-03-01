5045a366b85cdd16ee12efb705f59710f909ac25
Andres Rubiano <andres.rubiano.ch@gmail.com>
2024-02-29 23:52:39 -0500

    Added dots animation

--- app/src/main/java/com/example/projectcompose/MainActivity.kt
+++ app/src/main/java/com/example/projectcompose/MainActivity.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.projectcompose.examples.DotsPreview
 import com.example.projectcompose.ui.theme.ProjectComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -92,6 +93,7 @@ fun TestPreview() {
             MainScreen()
             MessageCard()
             MessageCardTwo()
+            DotsPreview()
         }
     }
 }
