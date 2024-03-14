07b1a4c52c0acfd6fc11d157d0bbf85043615da8
andres.rubiano.del.chiaro <andres.rubiano@globant.com>
2024-03-14 00:07:20 -0500

    Added Home screen Content to the main activity

--- app/src/main/java/com/example/projectcompose/MainActivity.kt
+++ app/src/main/java/com/example/projectcompose/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.projectcompose.codelabsBasicTheme.screens.HomeScreen
 import com.example.projectcompose.ui.theme.ProjectComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProjectComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen(Modifier)
                 }
             }
         }
@@ -29,8 +34,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+        text = "Hello $name!",
+        modifier = modifier
     )
 }
 
